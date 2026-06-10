a1026b21ed8efca6db0edd3634502c1cd67ac74e
emoajib-blip <emoajib@gmail.com>
2026-06-11 05:42:53 +0700

    fix: set PDF header LPPM and university text size to 11pt and merge university name to single line

--- resources/views/pdf/review-evaluation.blade.php
+++ resources/views/pdf/review-evaluation.blade.php
@@ -118,25 +118,19 @@
 
 <body>
     {{-- Vetted by AI - Manual Review Required by Senior Engineer/Manager --}}
-    <table class="header-table no-border">
+    <table class="header-table">
         <tr>
-            <td style="width: 95px; text-align: left; vertical-align: middle; border: none !important; padding: 0 !important;">
+            <td style="width: 105px; text-align: left; vertical-align: middle; border: none !important; padding: 0 !important;">
                 @if (get_logo_base64())
-                    <img src="{{ get_logo_base64() }}" alt="Logo" style="width: 85px; height: auto;">
+                    <img src="{{ get_logo_base64() }}" alt="Logo" style="width: 95px; height: auto;">
                 @endif
             </td>
             <td class="header-text" style="border: none !important; padding: 0 !important; vertical-align: middle; text-align: center;">
-                <div style="font-size: 14pt; font-weight: bold; text-transform: uppercase; line-height: 1.2; letter-spacing: 0.2px;">
-                    Lembaga Penelitian dan Pengabdian kepada
+                <div style="font-size: 11pt; font-weight: bold; text-transform: uppercase; line-height: 1.25; letter-spacing: 0.2px;">
+                    LPPM
                 </div>
-                <div style="font-size: 14pt; font-weight: bold; text-transform: uppercase; line-height: 1.2; letter-spacing: 0.2px;">
-                    Masyarakat (LPPM)
-                </div>
-                <div style="font-size: 14pt; font-weight: bold; text-transform: uppercase; line-height: 1.25; margin-top: 4px; letter-spacing: 0.2px;">
-                    Institut Teknologi dan Sains Nahdlatul
-                </div>
-                <div style="font-size: 14pt; font-weight: bold; text-transform: uppercase; line-height: 1.25; letter-spacing: 0.2px;">
-                    Ulama Pekalongan
+                <div style="font-size: 11pt; font-weight: bold; line-height: 1.25; margin-top: 4px; letter-spacing: 0.2px;">
+                    Institut Teknologi dan Sains Nahdlatul Ulama Pekalongan
                 </div>
                 <div style="font-size: 8pt; font-weight: normal; line-height: 1.3; margin-top: 6px; color: #111;">
                     Jl. Karangdowo No. 9, Karangdowo, Kec. Kedungwuni, Kab. Pekalongan, JATENG 51173
@@ -145,7 +139,7 @@
                     Email: lppmitsnupkl@gmail.com | Website: https://lppm.itsnupekalongan.ac.id/
                 </div>
             </td>
-            <td style="width: 95px; border: none !important; padding: 0 !important; vertical-align: middle;"></td>
+            <td style="width: 105px; border: none !important; padding: 0 !important; vertical-align: middle;"></td>
         </tr>
     </table>
 
